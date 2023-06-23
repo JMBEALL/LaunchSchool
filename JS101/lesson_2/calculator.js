1f5cfce1722f18fd749fde1b7f87ed2a0b1d450c
Jordan Beall <JordanBeall@me.com>
2023-06-23 13:17:25 -0700

    allow user to perform another calculation if wanted

--- JS101/lesson_2/calculator.js
+++ JS101/lesson_2/calculator.js
@@ -1,5 +1,11 @@
 const readline = require("readline-sync");
+let number1;
+let number2;
+let operation;
+let output;
+let newCalculation;
 
+//START
 /*prepending an arrow in front of each message the to create distinction*/
 
 function prompt(mes) {
@@ -10,54 +16,77 @@ function invalidNumber(num) {
   return Number.isNaN(num);
 }
 
-prompt("Welcome to Calculator!");
-//START
+function welcome() {
+  prompt("Welcome to Calculator!");
+}
 
+function firstInput() {
 //Ask the user for the first number
-prompt("What's the first number?");
-let number1 = Number(readline.question());
-
-while (invalidNumber(number1)) {
-  prompt("That does not look like a valid number. Please enter another number.");
+  prompt("What's the first number?");
   number1 = Number(readline.question());
-}
+
+  while (invalidNumber(number1)) {
+    prompt("That does not look like a valid number. Please enter another number.");
+    number1 = Number(readline.question());
+  }
 // console.log(number1)
+}
 //Ask the user for the second number
-prompt("what's the second number?");
-let number2 = Number(readline.question());
-
-while (invalidNumber(number2)) {
-  prompt("That does not look like a valid number. Please enter another number.");
+function secondInput() {
+  prompt("what's the second number?");
   number2 = Number(readline.question());
-}
 
+  while (invalidNumber(number2)) {
+    prompt("That does not look like a valid number. Please enter another number.");
+    number2 = Number(readline.question());
+  }
+}
 //Ask the user for the operation
-prompt("What operation would you like to perform on these two operands?\n1) Add 2) Subtract 3) Multiply 4) Divide");
-//Perform the operation and return the value
-let operation = readline.question();
+function operationChoice() {
+  prompt("What operation would you like to perform on these two operands?\n1) Add 2) Subtract 3) Multiply 4) Divide");
 
-while (!["1", "2", "3", "4"].includes(operation)) {
-  prompt("That operation is coming soon! Please review the operations currently supported and try again.");
   operation = readline.question();
+
+  while (!["1", "2", "3", "4"].includes(operation)) {
+    prompt("That operation is coming soon! Please review the operations currently supported and try again.");
+    operation = readline.question();
+  }
 }
 
-let output;
-
-switch (operation) {
-  case "1":
-    output = number1 + number2;
-    break;
-  case "2":
-    output = number1 - number2;
-    break;
-  case "3":
-    output = number1 * number2;
-    break;
-  case "4":
-    output = number1 / number2;
-    break;
+//Perform the operation and return the value
+function performOperation() {
+  switch (operation) {
+    case "1":
+      output = number1 + number2;
+      break;
+    case "2":
+      output = number1 - number2;
+      break;
+    case "3":
+      output = number1 * number2;
+      break;
+    case "4":
+      output = number1 / number2;
+      break;
+  }
+  prompt(`The result of your input is ${output}.`);
 }
 
-prompt(`The result of your input is ${output}.`);
+function calculator() {
+  welcome();
+  firstInput();
+  secondInput();
+  operationChoice();
+  performOperation();
+  newCalculation = readline.question("=> Would you like to perform another calculation? Y or N\n");
+}
+
+calculator();
+while ( newCalculation.toLocaleLowerCase() === "y" ) {
+  calculator();
+}
+//additional challenge- see if they want to perform another operation.
+//GET yes or no from user.
+// WHILE user says yes, continue to use the calculator
 
 //END
