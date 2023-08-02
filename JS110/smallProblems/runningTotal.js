37dfbba6d5d11e58eeb69a8a341fc048d662f147
Jordan Beall <JordanBeall@me.com>
2023-08-01 22:54:18 -0500

    review running totals

--- JS110/smallProblems/runningTotal.js
+++ JS110/smallProblems/runningTotal.js
@@ -1,7 +1,15 @@
 // Write a function that takes an array of numbers and returns an array with the same number of elements, but with each element's value being the running total from the original array.
 
-function runningTotal(arr){
-  
+function runningTotal(arr) {
+  if(arr.length === 0) {
+    return [];
+  }
+let runningTotals = [];
+  runningTotals.push(arr.shift());
+  arr.forEach((el, index) => {
+  return runningTotals.push(runningTotals[index] + el);
+})
+return runningTotals;
 }
 
 console.log(runningTotal([2, 5, 13]));             // [2, 7, 20]
