930cc50d8cf44d28038e61ced80e484c16d139fe
Jordan Beall <JordanBeall@me.com>
2023-08-12 16:17:18 -0500

    practice

--- practice.js
+++ practice.js
@@ -799,20 +799,132 @@ function range(start = 0, end = 1) {
 // console.log(wordCap('the javaScript language'));    // "The Javascript Language"
 // console.log(wordCap('this is a "quoted" word'));    // 'This Is A "quoted" Word'
 
-function swapCase(str) {
-let arr = str.split("");
-let newStr = "";
-arr.map(char => {
-    if (/[a-z]/.test(char)) {
-      newStr += char.toUpperCase()
-  } else if (/[A-Z]/.test(char)) {
-      newStr += char.toLowerCase();
-  } else {
-    newStr += char;
-  }
-})
-return newStr;
+// function swapCase(str) {
+// let arr = str.split("");
+// let newStr = "";
+// arr.map(char => {
+//     if (/[a-z]/.test(char)) {
+//       newStr += char.toUpperCase()
+//   } else if (/[A-Z]/.test(char)) {
+//       newStr += char.toLowerCase();
+//   } else {
+//     newStr += char;
+//   }
+// })
+// return newStr;
+// }
+
+// console.log(swapCase('CamelCase'));              // "cAMELcASE"
+// console.log(swapCase('Tonight on XYZ-TV'));      // "tONIGHT ON xyz-tv"
+
+// function staggeredCase(str) {
+//   let count = 0;
+//   return str.split("").map(char => {
+    
+//     if (/[A-Za-z]/.test(char)) {
+//       if (count % 2 === 0) {
+//         count++;
+//         return char.toUpperCase();
+//       } else {
+//         count++
+//         return char.toLowerCase();
+//       }
+//     } else {
+//       return char;
+//     }
+//   }).join("");
+// }
+
+// console.log(staggeredCase("I Love Launch School!") === "I lOvE lAuNcH sChOoL!");
+// console.log(staggeredCase("ALL CAPS") === "AlL cApS");
+// console.log(
+//   staggeredCase("ignore 77 the 444 numbers") === "IgNoRe 77 ThE 444 nUmBeRs"
+// );
+
+// console.log(staggeredCase('I Love Launch School!'));        // "I LoVe lAuNcH ScHoOl!"
+// console.log(staggeredCase('ALL_CAPS'));                     // "AlL_CaPs"
+// console.log(staggeredCase('ignore 77 the 4444 numbers'));   // "IgNoRe 77 ThE 4444 nUmBeRs"
+
+// function wordLengths(str) {
+//   if (str === "" || str === undefined){
+// return []
+//   } 
+//   return str.split(" ").map(word => `${word} ${word.length}`);
+// }
+
+// console.log(wordLengths('cow sheep chicken'));
+// // ["cow 3", "sheep 5", "chicken 7"]
+
+// console.log(wordLengths('baseball hot dogs and apple pie'));
+// // ["baseball 8", "hot 3", "dogs 4", "and 3", "apple 5", "pie 3"]
+
+// console.log(wordLengths("It ain't easy, is it?"));
+// // ["It 2", "ain't 5", "easy, 5", "is 2", "it? 3"]
+
+// console.log(wordLengths('Supercalifragilisticexpialidocious'));
+// // ["Supercalifragilisticexpialidocious 34"]
+
+// console.log(wordLengths(''));      // []
+// console.log(wordLengths());        // []
+
+// function searchWord(word , str) {
+// let regex = new RegExp(word, 'ig');
+//   // return str.split(" ").map(word => {
+//   //   if (word.match(regex)) {
+//   //     return `**${word.toUpperCase()}**`;
+//   //   } else {
+//   //     return word;
+//   //   }
+//   // }).join(" ");
+//   return str.replace(regex, `**${word.toUpperCase()}**`);
+// }
+
+// const text = 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?';
+
+// console.log(searchWord('sed', text));      
+
+// function removeVowels(arr) {
+//   return arr.map(word => {
+//     return word.replace(/[aeiouAEIOU]/g, "");
+//   })
+// }
+
+// console.log(removeVowels(['abcdefghijklmnopqrstuvwxyz']));         // ["bcdfghjklmnpqrstvwxyz"]
+// console.log(removeVowels(['green', 'YELLOW', 'black', 'white']));  // ["grn", "YLLW", "blck", "wht"]
+// console.log(removeVowels(['ABC', 'AEIOU', 'XYZ']));                // ["BC", "", "XYZ"]
+
+// function sequence(count, start) {
+//   let newArr = [];
+//   for (let index = 1 ; index <= count; index++) {
+//     newArr.push(start * index)
+//   }
+//   return newArr
+// }
+
+// console.log(sequence(5, 1));          // [1, 2, 3, 4, 5]
+// console.log(sequence(4, -7));         // [-7, -14, -21, -28]
+// console.log(sequence(3, 0));          // [0, 0, 0]
+// console.log(sequence(0, 1000000));    // []
+
+// function reverseSentence(str) {
+//   return str.split(" ").reverse().join(" ");
+// }
+
+// console.log(reverseSentence(''));                       // ""
+// console.log(reverseSentence('Hello World'));            // "World Hello"
+// console.log(reverseSentence('Reverse these words'));    // "words these Reverse"
+
+function reverseWords(str) {
+
+  return str.split(" ").map(word => {
+    if (word.length >= 5 ) {
+      return word.split("").reverse().join("");
+    } else {
+      return word;
+    }
+  }).join(" ")
 }
 
-console.log(swapCase('CamelCase'));              // "cAMELcASE"
-console.log(swapCase('Tonight on XYZ-TV'));      // "tONIGHT ON xyz-tv"
+console.log(reverseWords('Professional'));             // "lanoisseforP"
+console.log(reverseWords('Walk around the block'));    // "Walk dnuora the kcolb"
+console.log(reverseWords('Launch School'));            // "hcnuaL loohcS"
