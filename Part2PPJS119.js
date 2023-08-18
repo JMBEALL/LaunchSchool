7e03188034b9e7a9c6d9bb886e29409294468b3b
Jordan Beall <JordanBeall@me.com>
2023-08-18 18:14:24 -0500

    practice and review sort method

--- Part2PPJS119.js
+++ Part2PPJS119.js
@@ -265,12 +265,14 @@
 
 // }))
 
-let books = [
-  { title: 'One Hundred Years of Solitude', author: 'Gabriel Garcia Marquez', published: '1967' },
-  { title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', published: '1925' },
-  { title: 'War and Peace', author: 'Leo Tolstoy', published: '1869' },
-  { title: 'Ulysses', author: 'James Joyce', published: '1922' },
-  { title: 'The Book of Kells', author: 'Multiple Authors', published: '800' },
-];
+let arr = [['b', 'c', 'a'], [2, 11, -3], ['blue', 'black', 'green']];
 
-console.log(books.sort((a,b) => a.published - b.published))
+arr.map((arr) => {
+  if (typeof arr[0] === 'string'){
+    return arr.sort().reverse();
+  } else {
+    return arr.sort((a,b) => b - a);
+  }
+})
+
+console.log(arr);
