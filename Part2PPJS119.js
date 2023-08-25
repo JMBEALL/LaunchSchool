bdb26a1fa455610bfbad67e042a3e469de1c3937
Jordan Beall <JordanBeall@me.com>
2023-08-25 13:22:49 -0500

    review writing more consice code

--- Part2PPJS119.js
+++ Part2PPJS119.js
@@ -1271,22 +1271,32 @@ const readline = require('readline-sync');
 // );
 
 
-function wordLengths(str) {
-  if (str === "" || str === undefined) return [];
-  return str.split(" ").map(el => el + " " + el.length);
+// function wordLengths(str) {
+//   if (str === "" || str === undefined) return [];
+//   return str.split(" ").map(el => el + " " + el.length);
+// }
+
+// console.log(wordLengths('cow sheep chicken'));
+// // ["cow 3", "sheep 5", "chicken 7"]
+
+// console.log(wordLengths('baseball hot dogs and apple pie'));
+// // ["baseball 8", "hot 3", "dogs 4", "and 3", "apple 5", "pie 3"]
+
+// console.log(wordLengths("It ain't easy, is it?"));
+// // ["It 2", "ain't 5", "easy, 5", "is 2", "it? 3"]
+
+// console.log(wordLengths('Supercalifragilisticexpialidocious'));
+// // ["Supercalifragilisticexpialidocious 34"]
+
+// console.log(wordLengths(''));      // []
+// console.log(wordLengths());        // []
+
+function searchWord(word, str) {
+
+return str.split(" ").filter(el => el.toLowerCase() === word.toLowerCase()).length;
+
 }
 
-console.log(wordLengths('cow sheep chicken'));
-// ["cow 3", "sheep 5", "chicken 7"]
+const text = 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?';
 
-console.log(wordLengths('baseball hot dogs and apple pie'));
-// ["baseball 8", "hot 3", "dogs 4", "and 3", "apple 5", "pie 3"]
-
-console.log(wordLengths("It ain't easy, is it?"));
-// ["It 2", "ain't 5", "easy, 5", "is 2", "it? 3"]
-
-console.log(wordLengths('Supercalifragilisticexpialidocious'));
-// ["Supercalifragilisticexpialidocious 34"]
-
-console.log(wordLengths(''));      // []
-console.log(wordLengths());        // []
+console.log(searchWord('sed', text));      // 3
