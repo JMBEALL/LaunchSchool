4f5182667cbd9c12a2cfa4ad5849fe7ff4d53300
Jordan Beall <JordanBeall@me.com>
2023-08-25 13:16:20 -0500

    review and practice string and text processing

--- Part2PPJS119.js
+++ Part2PPJS119.js
@@ -1243,29 +1243,50 @@ const readline = require('readline-sync');
 // console.log(staggeredCase('ALL_CAPS'));                     // "AlL_CaPs"
 // console.log(staggeredCase('ignore 77 the 4444 numbers'));   // "IgNoRe 77 ThE 4444 nUmBeRs"
 
-function staggeredCase(str) {
-  let count = 0;
-  let toggle = 0
-  let newStr = "";
-  while (count < str.length) {
-    if (str[count].match(/[a-z]/i) && toggle % 2 === 0){{
-      newStr += str[count].toUpperCase()
-      count++;
-      toggle++;
-    }} else if (str[count].match(/[a-z]/i) && toggle % 2 !== 0) {
-      newStr += str[count].toLowerCase();
-      count++;
-      toggle++;
-    } else {
-      newStr += str[count];
-      count++
-    }
-  }
-  return newStr;
+// function staggeredCase(str) {
+//   let count = 0;
+//   let toggle = 0
+//   let newStr = "";
+//   while (count < str.length) {
+//     if (str[count].match(/[a-z]/i) && toggle % 2 === 0){{
+//       newStr += str[count].toUpperCase()
+//       count++;
+//       toggle++;
+//     }} else if (str[count].match(/[a-z]/i) && toggle % 2 !== 0) {
+//       newStr += str[count].toLowerCase();
+//       count++;
+//       toggle++;
+//     } else {
+//       newStr += str[count];
+//       count++
+//     }
+//   }
+//   return newStr;
+// }
+
+// console.log(staggeredCase("I Love Launch School!") === "I lOvE lAuNcH sChOoL!");
+// console.log(staggeredCase("ALL CAPS") === "AlL cApS");
+// console.log(
+//   staggeredCase("ignore 77 the 444 numbers") === "IgNoRe 77 ThE 444 nUmBeRs"
+// );
+
+
+function wordLengths(str) {
+  if (str === "" || str === undefined) return [];
+  return str.split(" ").map(el => el + " " + el.length);
 }
 
-console.log(staggeredCase("I Love Launch School!") === "I lOvE lAuNcH sChOoL!");
-console.log(staggeredCase("ALL CAPS") === "AlL cApS");
-console.log(
-  staggeredCase("ignore 77 the 444 numbers") === "IgNoRe 77 ThE 444 nUmBeRs"
-);
+console.log(wordLengths('cow sheep chicken'));
+// ["cow 3", "sheep 5", "chicken 7"]
+
+console.log(wordLengths('baseball hot dogs and apple pie'));
+// ["baseball 8", "hot 3", "dogs 4", "and 3", "apple 5", "pie 3"]
+
+console.log(wordLengths("It ain't easy, is it?"));
+// ["It 2", "ain't 5", "easy, 5", "is 2", "it? 3"]
+
+console.log(wordLengths('Supercalifragilisticexpialidocious'));
+// ["Supercalifragilisticexpialidocious 34"]
+
+console.log(wordLengths(''));      // []
+console.log(wordLengths());        // []
