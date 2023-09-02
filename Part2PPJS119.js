5bc4448037b2d0d5ff66612991b29d643be597b7
Jordan Beall <JordanBeall@me.com>
2023-09-02 15:34:07 -0500

    review and practice LS interview level questions

--- Part2PPJS119.js
+++ Part2PPJS119.js
@@ -2581,4 +2581,27 @@ const readline = require('readline-sync');
 
 
 
+function closestNumbers(arr) {
+  let min = [];
+  let closest = Infinity;
+  for(let index = 0; index < arr.length; index++) {
+    for (let j = 0; j < arr.length; j++) {
+      if (Math.abs(arr[index] - arr[j]) < closest && index !== j ) {
+        min = [arr[index], arr[j]];
+        closest = Math.abs(arr[index] - arr[j])
+      }
+    }
+  }
+  return min;
+}
 
+
+// Write a function that takes an array of integers and
+// returns the two numbers that are closest together in
+// value.
+
+// Examples:
+
+console.log(closestNumbers([5, 25, 15, 11, 20]));     // [15, 11]
+console.log(closestNumbers([19, 25, 32, 4, 27, 16])); // [25, 27]
+console.log(closestNumbers([12, 7, 17]));             // [12, 7]
