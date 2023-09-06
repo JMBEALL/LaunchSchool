beac10077a6e057651b1219b3ca0e4e30ff5c157
Jordan Beall <JordanBeall@me.com>
2023-09-06 08:49:19 -0500

    practice

--- PEDACStudySession.js
+++ PEDACStudySession.js
@@ -176,41 +176,192 @@ Single digit numbers will NOT be considered numerical palindromes.
     //if it is true; push it into the array
     //if not, move onto the next iteration;
 
-function palindrome(num, s) {
-  if ((typeof num === 'string' || typeof s === "string") || (num < 0 || s < 0)) {
-    return  "NOT VALID";
-  }
-  if (num < 10) {
-    num = 10;
-  }
-  let palindromArr = [];
-  for (let index = num; index < Infinity; index++) {
-    if (palindromArr.length === s) {
-      return palindromArr;
-    } else if ( index === +(String(index).split("").reverse().join(""))) {
-      palindromArr.push(index);
+// function palindrome(num, s) {
+//   if ((typeof num === 'string' || typeof s === "string") || (num < 0 || s < 0)) {
+//     return  "NOT VALID";
+//   }
+//   if (num < 10) {
+//     num = 10;
+//   }
+//   let palindromArr = [];
+//   for (let index = num; index < Infinity; index++) {
+//     if (palindromArr.length === s) {
+//       return palindromArr;
+//     } else if ( index === +(String(index).split("").reverse().join(""))) {
+//       palindromArr.push(index);
+//     }
+//   }
+// }
+
+
+// console.log(palindrome(6,4));
+// // [11,22,33,44]
+// console.log(palindrome(75,1));
+// // [77]
+// console.log(palindrome(101,2));
+// // [101,111]
+// console.log(palindrome(20,0));
+// // []
+// console.log(palindrome(0,4));
+// // [11,22,33,44]
+
+// console.log(palindrome("ACCDDCCA",3));
+// // "Not valid"
+// console.log(palindrome(773,"1551"));
+// // "Not valid"
+// console.log(palindrome(-4505,15));
+// // "Not valid"
+// console.log(palindrome(4505,-15));
+// "Not valid"
+
+
+// function sum(num) {
+//   return String(num).split("").reduce((accum,el) => accum + +el, 0)
+// }
+
+// console.log(sum(23));           // 5
+// console.log(sum(496));          // 19
+// console.log(sum(123456789));    // 45
+
+// function multiplyAllPairs(arr1,arr2) {
+//   let arr= [];
+//   for (let index = 0; index < arr1.length; index++) {
+//     for (let j = 0; j < arr2.length; j++) {
+//       arr.push(arr1[index] * arr2[j]);
+//     }
+//   }
+//   return arr.sort((a,b) => a - b);
+// }
+
+// console.log(multiplyAllPairs([2, 4], [4, 3, 1, 2]));    // [2, 4, 4, 6, 8, 8, 12, 16]
+
+// function leadingSubstrings(str) {
+//   let subs = [];
+//   for (let start = 0; start < str.length; start++) {
+//     subs.push(str.slice(0, start + 1));
+//   }
+//   return subs;
+// }
+
+// console.log(leadingSubstrings('abc'));      // ["a", "ab", "abc"]
+// console.log(leadingSubstrings('a'));        // ["a"]
+// console.log(leadingSubstrings('xyzzy'));    // ["x", "xy", "xyz", "xyzz", "xyzzy"]
+
+
+// function substrings(str) {
+//   let subs = [];
+//   for (let start = 0; start < str.length; start++) {
+//     for (let chars = 2; chars <= str.length - start; chars++) {
+//       subs.push(str.slice(start, start + chars));
+//     }
+//   }
+//   return subs;
+// }
+
+// function palindromes(str) {
+//   let answer = substrings(str);
+//   let final = [];
+//   answer.forEach(el => {
+//     if (isPalindrome(el)) {
+//       final.push(el)
+//     }
+//   })
+//   return final;
+// }
+
+// function isPalindrome(str){
+//   return str === str.split("").reverse().join("");
+// }
+
+// console.log(palindromes('abcd'));       // []
+// console.log(palindromes('madam'));      // [ "madam", "ada" ]
+
+// console.log(palindromes('hello-madam-did-madam-goodbye'));
+// // returns
+// // [ "ll", "-madam-", "-madam-did-madam-", "madam", "madam-did-madam", "ada",
+// //   "adam-did-mada", "dam-did-mad", "am-did-ma", "m-did-m", "-did-", "did",
+// //   "-madam-", "madam", "ada", "oo" ]
+
+// console.log(palindromes('knitting cassettes'));
+// // returns
+// // [ "nittin", "itti", "tt", "ss", "settes", "ette", "tt" ]
+
+
+// function sumOfSums(arr) {
+// let sum = 0;
+// for (let start = 0; start < arr.length; start++) {
+//   sum += arr.slice(0, start + 1).reduce((accum,el) => {
+//     return accum + el;
+//   })
+// }
+// return sum;
+// }
+
+// console.log(sumOfSums([3, 5, 2]));        // (3) + (3 + 5) + (3 + 5 + 2) --> 21
+// console.log(sumOfSums([1, 5, 7, 3]));     // (1) + (1 + 5) + (1 + 5 + 7) + (1 + 5 + 7 + 3) --> 36
+// console.log(sumOfSums([4]));              // 4
+// console.log(sumOfSums([1, 2, 3, 4, 5]));  // 35
+
+// function buyFruit(arr) {
+//   let arr1 = [];
+//   arr.forEach(sub => {
+//     console.log(sub);
+//     for (let index = 1; index <= sub[1]; index++) {
+//       arr1.push(sub[0]);
+//     }
+//   })
+//   return arr1;
+// }
+
+// console.log(buyFruit([['apple', 3], ['orange', 1], ['banana', 2]]));
+// // returns ["apple", "apple", "apple", "orange", "banana", "banana"]
+
+// function transactionsFor(id,arr) {
+// return arr.filter(obj => obj.id === id)
+// }
+
+
+// let transactions = [ { id: 101, movement: 'in',  quantity:  5 },
+//                      { id: 105, movement: 'in',  quantity: 10 },
+//                      { id: 102, movement: 'out', quantity: 17 },
+//                      { id: 101, movement: 'in',  quantity: 12 },
+//                      { id: 103, movement: 'out', quantity: 20 },
+//                      { id: 102, movement: 'out', quantity: 15 },
+//                      { id: 105, movement: 'in',  quantity: 25 },
+//                      { id: 101, movement: 'out', quantity: 18 },
+//                      { id: 102, movement: 'in',  quantity: 22 },
+//                      { id: 103, movement: 'out', quantity: 15 }, ];
+
+// console.log(transactionsFor(101, transactions));
+// // returns
+// // [ { id: 101, movement: "in",  quantity:  5 },
+// //   { id: 101, movement: "in",  quantity: 12 },
+// //   { id: 101, movement: "out", quantity: 18 }, ]
+
+
+function isItemAvailable(id, arr) {
+  let sum = 0;
+arr.filter(obj => obj.id === id).forEach(obj => {
+    if(obj.movement === "in") {
+      sum += obj.quantity
+    } else if (obj.movement === "out") {
+      sum -= obj.quantity
     }
-  }
+  })
+  return sum >= 0;
 }
 
+let transactions = [ { id: 101, movement: 'in',  quantity:  5 },
+                     { id: 105, movement: 'in',  quantity: 10 },
+                     { id: 102, movement: 'out', quantity: 17 },
+                     { id: 101, movement: 'in',  quantity: 12 },
+                     { id: 103, movement: 'out', quantity: 20 },
+                     { id: 102, movement: 'out', quantity: 15 },
+                     { id: 105, movement: 'in',  quantity: 25 },
+                     { id: 101, movement: 'out', quantity: 18 },
+                     { id: 102, movement: 'in',  quantity: 22 },
+                     { id: 103, movement: 'out', quantity: 15 }, ];
 
-console.log(palindrome(6,4));
-// [11,22,33,44]
-console.log(palindrome(75,1));
-// [77]
-console.log(palindrome(101,2));
-// [101,111]
-console.log(palindrome(20,0));
-// []
-console.log(palindrome(0,4));
-// [11,22,33,44]
-
-console.log(palindrome("ACCDDCCA",3));
-// "Not valid"
-console.log(palindrome(773,"1551"));
-// "Not valid"
-console.log(palindrome(-4505,15));
-// "Not valid"
-console.log(palindrome(4505,-15));
-// "Not valid"
-
+console.log(isItemAvailable(101, transactions));     // false
+console.log(isItemAvailable(103, transactions));     // false
+console.log(isItemAvailable(105, transactions));     // true
