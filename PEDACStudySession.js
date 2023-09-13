d14b2bab02bc0f28a6ccc4818bbd71db2fb1f89b
Jordan Beall <JordanBeall@me.com>
2023-09-13 09:22:28 -0500

    JS 119 Interview prep

--- PEDACStudySession.js
+++ PEDACStudySession.js
@@ -1955,13 +1955,15 @@ C:
 
 
 
-function solution(fullText, searchText) {
-let regex = new RegExp(searchText, "ig") 
-let arr = fullText.match(regex) || []
-return arr.length;
-
+function solve(s) {
+return Math.max(...(s.match(/[aeiou]+/g) || []).map(el => el.length));
 }
 
-console.log(solution('abcdeb','b'))//2
-console.log(solution('abc','b')) //1 
-console.log(solution('abbc','bb')) // 1
+console.log(solve("codewarriors"))//2
+console.log(solve("suoidea"))//3
+console.log(solve("ultrarevolutionariees"))//3
+console.log(solve("strengthlessnesses")) //1
+console.log(solve("cuboideonavicuare")) //2
+console.log(solve("chrononhotonthuooaos")) //5
+console.log(solve("iiihoovaeaaaoougjyaw"))//8
+
