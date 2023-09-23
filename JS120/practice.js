7459decfb15afb297acb71600c96fa0197aa6e6a
Jordan Beall <JordanBeall@me.com>
2023-09-22 21:45:54 -0500

    add and keep track of previous moves of each move in Object Oriented Rock, Paper, Scissors

--- JS120/practice.js
+++ JS120/practice.js
@@ -1,39 +1,53 @@
 const readline = require('readline-sync');
-
-
-
-function createPlayer(playerType) {
+function createHuman() {
   return {
-    playerType,
     move : null,
-    isHuman() {
-      return this.playerType === 'human';
-    },
+    previousMoves : [],
+    // isHuman() {
+    //   return this.playerType === 'human';
+    // },
     prompt(msg) {
       console.log(`=> ${msg}`);
     },
     choose() {
-      if (this.isHuman()) {
-        this.prompt(`Please choose one: rock, paper, or scissors...`);
-        let choice = readline.question().toLowerCase();
-        while (!['rock', 'paper', 'scissors'].includes(choice)) {
-          this.prompt("Please enter a valid entry...");
-          choice = readline.question();
-        }
-        this.move = choice.toLowerCase();
-      } else {
-        const choices = ['rock', 'paper', 'scissors'];
-        let randomNum = Math.floor(Math.random() * choices.length);
-        this.move = choices[randomNum];
+      this.prompt(`Please choose one: rock, paper, or scissors...`);
+      let choice = readline.question().toLowerCase();
+      while (!['rock', 'paper', 'scissors'].includes(choice)) {
+        this.prompt("Please enter a valid entry...");
+        choice = readline.question();
       }
+      this.move = choice.toLowerCase();
+      this.previousMoves.push(choice);
+      // console.log(this.previousMoves);
+    }
+  };
+}
+
+function createComputer() {
+  return {
+    move : null,
+    previousMoves : [],
+    // isHuman() {
+    //   return this.playerType === 'human';
+    // },
+    prompt(msg) {
+      console.log(`=> ${msg}`);
+    },
+    choose() {
+      let choices = ['rock', 'paper', 'scissors'];
+      let randomNumber = Math.floor(Math.random() * choices.length);
+      let choice = choices[randomNumber];
+      this.move = choice;
+      this.previousMoves.push(choice);
+      // console.log(this.previousMoves);
     }
   };
 }
 
 
 const RPSGame = {
-  human: createPlayer('human'),
-  computer: createPlayer('computer'),
+  human: createHuman(),
+  computer: createComputer(),
 
   displayWelcomeMessage() {
     console.log(`Welcome to Rock, Paper, Scissors on Jordan's MacBook Pro. Best of luck!`);
@@ -43,28 +57,28 @@ const RPSGame = {
     console.log(`Thanks for playing Rock, Paper, Scissors. See you next time!`);
   },
   displayWinner() {
-      let humanMove = this.human.move;
-      let computerMove = this.computer.move;
-    
-      console.log(`You chose: ${humanMove}.`);
-      console.log(`The computer chose: ${computerMove}.`);
-    
-      if ((humanMove === 'rock' && computerMove === 'scissors') ||
+    let humanMove = this.human.move;
+    let computerMove = this.computer.move;
+    let humanMoves = this.human.previousMoves ;
+    let computerMoves = this.computer.previousMoves ;
+    console.log(`You chose: ${humanMove}. Your previous moves are ${humanMoves.join(", ")}.`);
+    console.log(`The computer chose: ${computerMove}. The computer has chosen ${computerMoves.join(", ")}.`);
+    if ((humanMove === 'rock' && computerMove === 'scissors') ||
           (humanMove === 'paper' && computerMove === 'rock') ||
           (humanMove === 'scissors' && computerMove === 'paper')) {
-        console.log('You win!');
-      } else if ((humanMove === 'rock' && computerMove === 'paper') ||
+      console.log('You win!');
+    } else if ((humanMove === 'rock' && computerMove === 'paper') ||
                  (humanMove === 'paper' && computerMove === 'scissors') ||
                  (humanMove === 'scissors' && computerMove === 'rock')) {
-        console.log('Computer wins!');
-      } else {
-        console.log("It's a tie");
-      }
+      console.log('Computer wins!');
+    } else {
+      console.log("It's a tie");
+    }
   },
   playAgain() {
     console.log('Would you like to play again? (y/n)');
     let answer = readline.question();
-      return answer === 'y';
+    return answer === 'y';
 
   },
   play() {
