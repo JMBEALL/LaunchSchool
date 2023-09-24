869f11d10513d6c5e3d504b50bb71b31680c3f63
Jordan Beall <JordanBeall@me.com>
2023-09-24 17:49:16 -0500

    review factory functions in OOP

--- JS120/practice.js
+++ JS120/practice.js
@@ -172,14 +172,103 @@ const RPSGame = {
 
 // logReturnVal(turk.getDescription, turk);
 
-const TESgames = {
-  titles: ['Arena', 'Daggerfall', 'Morrowind', 'Oblivion', 'Skyrim'],
-  seriesTitle: 'The Elder Scrolls',
-  listGames: function() {
-    this.titles.forEach(title => {
-      console.log(this.seriesTitle + ': ' + title);
-    });
-  }
-};
+// const TESgames = {
+//   titles: ['Arena', 'Daggerfall', 'Morrowind', 'Oblivion', 'Skyrim'],
+//   seriesTitle: 'The Elder Scrolls',
+//   listGames: function() {
+//     this.titles.forEach(title => {
+//       console.log(this.seriesTitle + ': ' + title);
+//     });
+//   }
+// };
 
-TESgames.listGames();
+// TESgames.listGames();
+
+// let foo = {
+//   a: 0,
+//   incrementA: function() {
+//   () => {
+//       this.a += 1;
+//     }
+
+//   }
+// };
+
+// foo.incrementA();
+// foo.incrementA();
+// foo.incrementA();
+
+// console.log(foo.a)
+
+function createInvoice(obj = {}) {
+  return {
+    phone : (obj.phone ? obj.phone : 3000),
+    internet : (obj.internet ? obj.internet : 5500 ),
+    total() {
+      return this.phone + this.internet;
+    }
+  }
+}
+
+
+function invoiceTotal(invoices) {
+  let total = 0;
+
+  for (let index = 0; index < invoices.length; index += 1) {
+    total += invoices[index].total();
+  }
+
+  return total;
+}
+
+let invoices = [];
+invoices.push(createInvoice());
+invoices.push(createInvoice({ internet: 6500 }));
+invoices.push(createInvoice({ phone: 2000 }));
+invoices.push(createInvoice({
+  phone: 1000,
+  internet: 4500,
+}));
+
+console.log(invoiceTotal(invoices)); // 31000
+
+function createPayment(services = {}) {
+return {
+  phone : services.phone ? services.phone : 0,
+  internet : services.internet ? services.internet : 0,
+  amount: services.amount ? services.amount : 0,
+
+  total() {
+    if (services.amount) {
+      return this.amount;
+    } else {
+      return this.phone + this.internet;
+    }
+  }
+}
+}
+
+function paymentTotal(payments) {
+  return payments.reduce((sum, payment)  => sum + payment.total(), 0);
+}
+
+let payments = [];
+payments.push(createPayment());
+payments.push(createPayment({
+  internet: 6500,
+}));
+
+payments.push(createPayment({
+  phone: 2000,
+}));
+
+payments.push(createPayment({
+  phone: 1000,
+  internet: 4500,
+}));
+
+payments.push(createPayment({
+  amount: 10000,
+}));
+
+console.log(paymentTotal(payments));      // => 24000
