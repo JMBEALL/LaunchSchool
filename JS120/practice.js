620582a14a94f52a14f6c36c84fcc977a4abbeec
Jordan Beall <JordanBeall@me.com>
2023-09-30 19:23:51 -0500

    ls js oo problems

--- JS120/practice.js
+++ JS120/practice.js
@@ -446,3 +446,12 @@ let rect = new Rectangle(4, 5);
 console.log(rect.getWidth()); // 4
 console.log(rect.getLength()); // 5
 console.log(rect.getArea()); // 20
+
+class Square extends Rectangle {
+  constructor(size) {
+    super(size,size);
+  }
+}
+
+let square = new Square(5);
+console.log(`area of square = ${square.getArea()}`); // area of square = 25
