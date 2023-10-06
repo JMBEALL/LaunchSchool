71cfc52444a1720d92f0a1cff9a9a4a7b48ef098
Jordan Beall <JordanBeall@me.com>
2023-10-06 18:51:47 -0500

    practice

--- JS120/practice.js
+++ JS120/practice.js
@@ -619,23 +619,65 @@ function createStudent (name, year) {
 // newCar.start();
 // console.log(newCar);
 
+//class Declaration - not hoisted, so are set to undefined until the code tells it what to set the value as, until it gets to that code, it is living in the Temporal Dead Zone in JS.
 
-let PetPrototype = {
-  init(animal, name) {
-    this.animal = animal;
+
+class Pet {
+  constructor(pet , name) {
+    this.pet = pet;
     this.name = name;
-    return this;
-  },
-  sleep() {
-    console.log("I am sleeping");
-  },
-  wake() {
-    console.log("I am awake!");
   }
 }
 
-let pudding = Object.create(PetPrototype).init("Cat", "Pudding");
-console.log(pudding);
+class Owner {
+  constructor(name) {
+    this.name = name;
+    this.arr = [];
+    Owner.keepTrack.push(this);
+  }
+  numberOfPets() {
+    return this.arr.length;
+  }
 
-pudding.sleep();
+  static keepTrack = [];
+}
 
+class Shelter  {
+  adopt(name, animalName) {
+   name.arr.push(animalName);
+  }
+
+  printAdoptions() {
+    // console.log(`${} has adopted the following pets:`);
+    Owner.keepTrack.forEach(Ownobj => {
+      console.log(`${Ownobj.name} has adopted the following pets:`)
+      Ownobj.arr.forEach(obj => {
+        console.log(`a ${obj.pet} named ${obj.name}.`);
+      })
+    })
+  }
+}
+
+let butterscotch = new Pet('cat', 'Butterscotch');
+let pudding      = new Pet('cat', 'Pudding');
+let darwin       = new Pet('bearded dragon', 'Darwin');
+let kennedy      = new Pet('dog', 'Kennedy');
+let sweetie      = new Pet('parakeet', 'Sweetie Pie');
+let molly        = new Pet('dog', 'Molly');
+let chester      = new Pet('fish', 'Chester');
+
+let phanson = new Owner('P Hanson');
+let bholmes = new Owner('B Holmes');
+
+let shelter = new Shelter();
+shelter.adopt(phanson, butterscotch);
+shelter.adopt(phanson, pudding);
+shelter.adopt(phanson, darwin);
+shelter.adopt(bholmes, kennedy);
+shelter.adopt(bholmes, sweetie);
+shelter.adopt(bholmes, molly);
+shelter.adopt(bholmes, chester);
+shelter.printAdoptions();
+// console.log(`${phanson.name} has ${phanson.numberOfPets()} adopted pets.`);
+// console.log(`${bholmes.name} has ${bholmes.numberOfPets()} adopted pets.`);
+// console.log(Owner.keepTrack)
