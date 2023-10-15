6e2a45d44a8f035ad0cbd25637db3731efb541ce
Jordan Beall <JordanBeall@me.com>
2023-10-15 15:54:06 -0500

    oop practice

--- JS120/practice.js
+++ JS120/practice.js
@@ -972,18 +972,55 @@ Second thought after thinking:
 //   }
 // }
 
+let walkMixIn = {
+  walk() {
+    console.log(`${this.name} ${this.gait()} forward`);
+  }
+}
 
+
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+
+  gait() {
+    return "strolls";
+  }
+}
 
 class Cat {
   constructor(name) {
     this.name = name;
   }
-  speaks() {
-    return `${this.name} says meowwww.`;
+
+  gait() {
+    return "saunters";
   }
 }
 
-let fakeCat = Object.create(Cat.prototype);
-console.log(fakeCat instanceof Cat); // logs true
-console.log(fakeCat.name);           // logs undefined
-console.log(fakeCat.speaks());       // logs undefined says meowwww.
+class Cheetah {
+  constructor(name) {
+    this.name = name;
+  }
+
+  gait() {
+    return "runs";
+  }
+}
+
+Object.assign(Person.prototype, walkMixIn);
+Object.assign(Cat.prototype, walkMixIn);
+Object.assign(Cheetah.prototype, walkMixIn);
+
+let mike = new Person("Mike");
+console.log(mike.walk());
+// "Mike strolls forward"
+
+let kitty = new Cat("Kitty");
+console.log(kitty.walk());
+// "Kitty saunters forward"
+
+let flash = new Cheetah("Flash");
+console.log(flash.walk());
+// "Flash runs forward"
