2f874db347c9be594cd555eaafb72ac4ed91d0ab
Jordan Beall <JordanBeall@me.com>
2023-10-20 08:14:14 -0500

    borrowing array methods for strings thanks to OOP

--- JS120/practice.js
+++ JS120/practice.js
@@ -1013,25 +1013,11 @@ Second thought after thinking:
 //   name : "BOOOOO BITCH!"
 // }
 
-function createPet(animal, name) {
-  return {
-    animal,
-    name,
-    sleep() {
-      console.log(`I am sleeping`);
-    },
-    wake() {
-      console.log(`I am awake.`);
-    }
-  }
-}
 
-let pudding = createPet("Cat", "Pudding");
-console.log(`I am a ${pudding.animal}. My name is ${pudding.name}.`);
-pudding.sleep(); // I am sleeping
-pudding.wake();  // I am awake
+let string = "jordan";
+let callBack = (el) => el.toUpperCase();
+let final = [1,2,3].map.call(string, callBack);
+console.log(final);
 
-let neptune = createPet("Fish", "Neptune");
-console.log(`I am a ${neptune.animal}. My name is ${neptune.name}.`);
-neptune.sleep(); // I am sleeping
-neptune.wake();  // I am awake
+
+
