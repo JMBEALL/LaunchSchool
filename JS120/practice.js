cd387d6cfce195861f582c8d1a03d36ff171abfb
Jordan Beall <JordanBeall@me.com>
2023-10-30 20:07:02 -0500

    writing my own reduce method

--- JS120/practice.js
+++ JS120/practice.js
@@ -1673,12 +1673,49 @@ function map(arr, callback) {
   return mappedArr;
 }
 
-let numbers = [1, 2, 3, 4, 5];
-console.log(map(numbers, number => number * 3));  // => [ 3, 6, 9, 12, 15 ]
-console.log(map(numbers, number => number + 1));  // => [ 2, 3, 4, 5, 6 ]
-console.log(map(numbers, () => false));
-// => [ false, false, false, false, false ]
+// let numbers = [1, 2, 3, 4, 5];
+// console.log(map(numbers, number => number * 3));  // => [ 3, 6, 9, 12, 15 ]
+// console.log(map(numbers, number => number + 1));  // => [ 2, 3, 4, 5, 6 ]
+// console.log(map(numbers, () => false));
+// // => [ false, false, false, false, false ]
 
-let values = [1, "abc", null, true, undefined, "xyz"];
-console.log(map(values, value => String(value)));
-// => [ '1', 'abc', 'null', 'true', 'undefined', 'xyz' ]
+// let values = [1, "abc", null, true, undefined, "xyz"];
+// console.log(map(values, value => String(value)));
+// // => [ '1', 'abc', 'null', 'true', 'undefined', 'xyz' ]
+
+
+
+// let arr = [1,2,3,4,5];
+
+// // let answer = arr.reduce((accum, el) => accum + el);
+// let answer = arr.reduce((arr , currentEl) => {
+//   if (!(currentEl <= 3)) {
+//     arr.push(currentEl * 2);
+//   }
+//   return arr;
+// }, []);
+
+// console.log(answer);
+
+function reduce(arr, callback, startVal) {
+  let start = startVal || arr[0];
+  for (let index = 0; index < arr.length; index++) {
+    start = callback(start, arr[index]);
+  }
+  return start;
+}
+
+let numbers = [1, 2, 3, 4, 5];
+console.log(reduce(numbers, (accum, number) => accum + number));   // => 15
+console.log(reduce(numbers, (prod, number) => prod * number));     // => 120
+console.log(reduce(numbers, (prod, number) => prod * number, 3));  // => 360
+console.log(reduce([], (accum, number) => accum + number, 10));    // => 10
+console.log(reduce([], (accum, number) => accum + number));
+// => undefined
+
+let stooges = ["Mo", "Larry", "Curly"];
+console.log(reduce(stooges, (reversedStooges, stooge) => {
+  reversedStooges.unshift(stooge);
+  return reversedStooges;
+}, []));
+// => ["Curly", "Larry", "Mo"]
